c8a03a29e6933da87cabf7307957a7690e81c7c8
didster42 <dhruvraja24@gmail.com>
2024-10-19 20:26:40 +0530

    Changes for demo

--- src/backend/internals/handler/message_handler.py
+++ src/backend/internals/handler/message_handler.py
@@ -1,3 +1,4 @@
+from src import chatbot_instance
 from src.backend.internals.modules.translation_module import translate_text_to_english
 from src.backend.internals.modules.whatsapp_module import send_whatsapp_message
 
@@ -17,9 +18,9 @@ def handle_whatsapp_message(body: dict):
 
 def handle_text_message(body: dict):
     # translated_text,language = translate_text_to_english(body.get('entry')[0].get('messages')[0].get('text').get('body'))
-    # psi_llm_response = rajan_fn()
+    psi_llm_response = chatbot_instance.respond(body.get('entry')[0].get('changes')[0].get('value').get('messages')[0].get('text').get('body'))
     # reverse_translation = translate_text_to_language(psi_llm_response, language)
-    return 'hi hello this is a test'
+    return psi_llm_response
 
 def handle_audio_message(body: dict):
     return ''
--- src/backend/orchestra_framework.py
+++ src/backend/orchestra_framework.py
@@ -3,13 +3,13 @@ import dspy
 
 
 
-chatbot = Chatbot_M(vanilla_llama_model = bedrock_model, finetuned_llama_model=knowledge_model)
+# chatbot = Chatbot_M(vanilla_llama_model = bedrock_model, finetuned_llama_model=knowledge_model)
 
-
-while True : 
-
-    user_input = input("User : ").strip()
-
-    agent_output = chatbot.respond(user_input)
-
-    print("Agent : {}".format(agent_output))
+#
+# while True :
+#
+#     user_input = input("User : ").strip()
+#
+#     agent_output = chatbot.respond(user_input)
+#
+#     print("Agent : {}".format(agent_output))
